8fee1b5a096f4fdf5c684c74a2159073ed4d55f4
Sisikelelwe Gomo <53611592+Sisikelelwe-Gomo@users.noreply.github.com>
2021-06-25 17:39:21 +0200

    Add files via upload

---
+++ endl keyword/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    num = 50;
+
+    cout << "number is =" << num << endl;
+    //endl is manipulator
+    //endl is output manipulator
+    cout << "valu of number=" << num << "\n";
+    return 0;
+}
